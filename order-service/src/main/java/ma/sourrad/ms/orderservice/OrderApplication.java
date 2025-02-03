0fb2c5dc3e7be3de2d42e6c025def463124278a6
Said OURRAD <ourrad.said.dev@gmail.com>
2025-02-03 23:21:54 +0100

    initial commit

--- order-service/src/main/java/ma/sourrad/ms/orderservice/OrderApplication.java
+++ order-service/src/main/java/ma/sourrad/ms/orderservice/OrderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeeApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 
 }
